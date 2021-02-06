092efb749cc92dca5509a4342f45293225cc6475
Christopher Dacuya <dacuych@mliph.manulife.com.ph>
2021-02-06 15:47:31 +0800

    Service-01 - Initial Commit
    - generated base class using spring initializer
    
    Signed-off-by: Christopher Dacuya <dacuych@mliph.manulife.com.ph>

---
+++ dice-service/src/test/java/com/avaloq/exercise/diceservice/DiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaloq.exercise.diceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
